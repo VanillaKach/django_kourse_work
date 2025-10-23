e9361ac409c369805d3f0e1316dde83ef76f05a6
VanillaKach <4d4m5@mail.ru>
2025-10-23 20:30:16 +0300

    Внёс правки по Redis, добавил шаблон к профилю. Расширил README.md по запуску проекта.

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,7 @@ AUTH_PASSWORD_VALIDATORS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv('REDIS_URL'),
+        "LOCATION": os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
